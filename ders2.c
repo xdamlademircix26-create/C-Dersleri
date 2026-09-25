1e647779d5da63d5d1ace0761615adb187e764dd
Damla Demirci <xdamlademircix26@gmail.com>
2026-09-25 14:35:40 +0300

    Hesaplama kodu eklendi

---
+++ ders2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() { 
+    int sayi1 = 15;
+    int sayi2 = 25;
+    int toplam = sayi1 + sayi2 ;
+
+    printf("Sayilarin Toplami: %d\n" , toplam);
+
+    return 0;
+}
