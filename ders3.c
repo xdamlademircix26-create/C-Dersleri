f4a50d4af2eeb73325ddeb97741b0368f321ee26
Damla Demirci <xdamlademircix26@gmail.com>
2026-09-25 15:07:35 +0300

    Kullanicidan veri alma ders3 tamamlandi

---
+++ ders3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() { 
+    int sayi1, sayi2, toplam;
+
+    printf("Birinci sayiyi giriniz: ");
+    scanf("%d", &sayi1);
+
+    printf("Ikinci sayiyi giriniz: ");
+    scanf("%d", &sayi2);
+
+    toplam = sayi1 + sayi2;
+    printf("Girdiginiz sayilarin toplami: %d\n", toplam);
+
+    return 0;
+     }
