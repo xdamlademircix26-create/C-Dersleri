f5614598220ff1d627cdb88543b8cf1bcad5379c
Damla Demirci <xdamlademircix26@gmail.com>
2026-09-26 14:17:33 +0300

    Hesaplanma kodu eklendi

---
+++ practice2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() { 
+    int sayi1 = 59;
+    int sayi2 = 72;
+    int toplam = sayi1 + sayi2 ;
+
+    printf("Sayilarin Toplami: %d\n" , toplam);
+    
+    return 0;
+}
+
+
