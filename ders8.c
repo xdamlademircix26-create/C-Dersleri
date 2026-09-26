8e6a065450ecc0d6d652395bcc7d5fd1d44843c7
Damla Demirci <xdamlademircix26@gmail.com>
2026-09-26 18:24:29 +0300

    Ondalikli sayilarda karisik islem tamamlandi

---
+++ ders8.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() { 
+    float sayi1, sayi2, sayi3, sonuc;
+
+    printf("Birinci sayiyi giriniz: ");
+    scanf("%f", &sayi1);
+
+    printf("Ikinci sayiyi giriniz: ");
+    scanf("%f", &sayi2);
+
+    printf("Cikarilacak ucuncu sayiyi giriniz: ");
+    scanf("%f", &sayi3);
+
+    sonuc = (sayi1 + sayi2) - sayi3;
+    printf("Islem sonucu: %.2f\n", sonuc);
+
+    return 0;
+}
