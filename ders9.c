21c2214af81d0eb9ab09b73ef4c26f428162b1a4
Damla Demirci <xdamlademircix26@gmail.com>
2026-09-26 18:37:14 +0300

    Carpim islemi tamamlandi

---
+++ ders9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() { 
+    int sayi1, sayi2, carpim;
+
+    printf("Birinci sayiyi giriniz: ");
+    scanf("%d", &sayi1);
+
+    printf("Ikinci sayiyi giriniz: ");
+    scanf("%d", &sayi2);
+
+    carpim = sayi1 * sayi2;
+    printf("Carpim sonucu: %d\n",carpim);
+
+    return 0;
+}
