ee107ba4a7800b8489ee3632465ce9e0dce01497
Damla Demirci <xdamlademircix26@gmail.com>
2026-09-26 15:32:48 +0300

    Ondalikli sayilarda toplama ders4 tamamlandi

---
+++ ders4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() { 
+    float sayi1, sayi2, toplam;
+
+    printf("Birinci sayiyi giriniz: ");
+    scanf("%f", &sayi1);
+
+    printf("Ikinci sayiyi giriniz: ");
+    scanf("%f", &sayi2);
+
+    toplam = sayi1 + sayi2;
+    printf("Girdiginiz sayilarin toplami: %f\n", toplam);
+
+    return 0;
+}
